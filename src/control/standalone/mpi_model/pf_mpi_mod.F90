dcfbbc4b83765529fcda7bd28cf2244e1e6b15fd
purplecat7 <j.p.lewis@reading.ac.uk>
2015-03-19 12:45:48 +0000

    Second integration iteration

--- src/control/standalone/mpi_model/pf_mpi_mod.F90
+++ src/control/standalone/mpi_model/pf_mpi_mod.F90
@@ -14,9 +14,9 @@ MODULE pf_mpi_mod
     INTEGER :: empi_err
     INTEGER :: empi_status(MPI_STATUS_SIZE)
     
-    INTEGER :: coupling_comms
-    INTEGER :: total_procs_coupled, coupling_id !found from size() and rank() calls in the setup
-    INTEGER :: num_models
+    INTEGER :: coupling_comms, pf_mpi_comm
+    INTEGER :: total_procs_coupled, filter_coupling_id !found from size() and rank() calls in the setup
+    INTEGER :: num_models, num_filters
 
     !This constant should reflect the number of state vectors which are being shared with the model
     INTEGER, PARAMETER :: states_2D = 3
@@ -24,7 +24,8 @@ MODULE pf_mpi_mod
     REAL(kind = KIND(1.0D0)), ALLOCATABLE, DIMENSION(:) :: state_vector
     INTEGER :: num_state_params !set in model_hook_up and used in the data transfer
 
-    INTEGER :: filter_id              !only used in model_rank_id assignment but useful for printouts later
+    INTEGER :: m_filter_id_rank
+    INTEGER :: filter_id        !only used in model_rank_id assignment but useful for printouts later
     INTEGER :: model_rank_id    !identifier of model instance
     INTEGER, ALLOCATABLE, DIMENSION(:) :: models !array for holding model_rank_id refs
     LOGICAL, ALLOCATABLE, DIMENSION(:) :: end_model_runs !array for accumulating the finish flag of each model process
@@ -37,30 +38,42 @@ CONTAINS
 
     SUBROUTINE empi_pf_initialise()
         IMPLICIT NONE
-        INTEGER, PARAMETER :: c_coupling_colour = 9999 !, c_pf_colour = 10000 !unused
-        INTEGER :: glbl_id
+        INTEGER, PARAMETER :: c_coupling_colour = 9999, c_pf_colour = 1
+        INTEGER :: world_id, world_size
 
         CALL MPI_INIT(empi_err)
 
-        PRINT *, 'PF: MPI initialised.'
+        PRINT *, 'PF: MPI initialising.'
 
         !Get the global id on the world communicator (which includes all the models too)
-        CALL MPI_COMM_RANK(MPI_COMM_WORLD, glbl_id, empi_err)
-
-        ! this split() is completely unnecessary since the communicator is never used
-        !CALL MPI_COMM_SPLIT(MPI_COMM_WORLD, c_pf_colour, pf_id, pf_mpi_comm, empi_err)
-        !PRINT*, MPI_COMM_WORLD, c_pf_colour, pf_id, pf_mpi_comm, empi_err
-
+        CALL MPI_COMM_RANK(MPI_COMM_WORLD, world_id, empi_err)
+        !Get the total number of processes running in this MPI environment
+        CALL MPI_COMM_SIZE(MPI_COMM_WORLD, world_size, empi_err)
+        
+        ! this split() is only used to find 'm_filter_id_rank'
+        CALL MPI_COMM_SPLIT(MPI_COMM_WORLD, c_pf_colour, world_id, pf_mpi_comm, empi_err)
+!        PRINT*, MPI_COMM_WORLD, c_pf_colour, world_id, pf_mpi_comm, empi_err
+        
+        !get the rank of this process on the communicator
+        CALL MPI_COMM_RANK(pf_mpi_comm, m_filter_id_rank, empi_err) !m_filter_id_rank==pfrank
+        PRINT*, 'rank done'
+        CALL MPI_COMM_SIZE(pf_mpi_comm, num_filters, empi_err) !num_filters==npfs
+        PRINT*, 'size done'
+        
         ! create/get a communicator for the model-filter coupler
-        CALL MPI_COMM_SPLIT(MPI_COMM_WORLD, c_coupling_colour, glbl_id, coupling_comms, empi_err)
-
-        ! how many processes are on the coupling communicator?
+        CALL MPI_COMM_SPLIT(MPI_COMM_WORLD, c_coupling_colour, world_id, coupling_comms, empi_err)
+        PRINT*, 'split done'
+        ! what is our id on the coupling communicator?
+        CALL MPI_COMM_RANK(coupling_comms, filter_coupling_id, empi_err)
+        PRINT*, 'rank done'
+        ! and how many processes are on it?
         CALL MPI_COMM_SIZE(coupling_comms, total_procs_coupled, empi_err)
-        ! and what is our id on it?
-        CALL MPI_COMM_RANK(coupling_comms, coupling_id, empi_err)
+        PRINT*, 'size done'
+       
+
 
         PRINT *, "PF empi_pf_initialise: Total coupled processes ", total_procs_coupled, &
-                    ", coupling rank id ", coupling_id, &
+                    ", coupling rank id ", filter_coupling_id, &
                     ", coupling comm id ", coupling_comms
 
     END SUBROUTINE empi_pf_initialise
@@ -71,25 +84,17 @@ CONTAINS
 
 
     SUBROUTINE empi_pf_assign_models()
-        USE pf_control !holds the datatype to store various pertinent information
         IMPLICIT NONE
-        INTEGER :: num_particle_filters, total_models, rtmp
-        INTEGER :: count, xnens, npfs, particle
-        INTEGER, PARAMETER :: c_particle_filter_flag_yes = 1
+        INTEGER :: total_models, rtmp, count
 
         PRINT *, 'PF: assigning models '
 
-        CALL MPI_ALLREDUCE(c_particle_filter_flag_yes, num_particle_filters, 1, MPI_INTEGER, MPI_SUM, coupling_comms, empi_err)
-
-        total_models = total_procs_coupled - num_particle_filters
-
-        ! note: this assumes that the particle filters are always higher ranking than the models - DEPENDENCY WHICH NEEDS TO BE REMOVED FROM THE LOGIC
-        filter_id = coupling_id - total_models
+        total_models = total_procs_coupled - num_filters
 
         !let's find the models:
-        !for simplicity, we can merely allocate an array that will be big enough for the largest
+        !for simplicity, we can merely allocate an array that will be big enough for the largest:
         !round up from number_of_models / number_of_filters
-        rtmp = CEILING(real(total_models)/real(num_particle_filters))
+        rtmp = CEILING(real(total_models)/real(num_filters))
         PRINT *, 'array allocation ', rtmp
         !allocate exact or one over number needed but initialize to -1 so that we don't use the extra one
         ALLOCATE(models(rtmp)) !ie the number of models attached to this pf process
@@ -101,8 +106,8 @@ CONTAINS
 
         rtmp = 1
         DO model_rank_id= 0, total_models-1
-            IF (real(model_rank_id) .ge. real(total_models * (filter_id))/real(num_particle_filters) .and.&
-                & real(model_rank_id) .lt. real(total_models * (filter_id + 1))/real(num_particle_filters)) THEN
+            IF (real(model_rank_id) .ge. real(total_models * (m_filter_id_rank))/real(num_filters) .and.&
+                & real(model_rank_id) .lt. real(total_models * (m_filter_id_rank + 1))/real(num_filters)) THEN
                 models(rtmp) = model_rank_id!put this before increment and then we don't need the -1 everywhere
                 rtmp = rtmp + 1
             END IF
@@ -110,18 +115,14 @@ CONTAINS
         num_models = rtmp-1
 
         PRINT *, 'Total coupled processes = ', total_procs_coupled
-        PRINT *, 'Number of filters = ', num_particle_filters
+        PRINT *, 'Number of filters = ', num_filters
         PRINT *, '-> Number of models = ', total_models
-        PRINT *, 'Coupling rank id = ', coupling_id
-        PRINT *, "-> Particle filter local relative rank = ", filter_id, " and I own"
-        DO rtmp=1, num_models
-            PRINT*, " model particle: " , models(rtmp)
-            PRINT*, 'end_model_run: ', end_model_runs(rtmp)
+        PRINT *, 'Coupling rank id = ', filter_coupling_id
+        PRINT *, "-> Particle filter local relative rank = ", m_filter_id_rank, " and I own"
+        DO count=1, num_models
+            PRINT*, " model particle: " , models(count)
+            PRINT*, 'end_model_run: ', end_model_runs(count)
         END DO
-
-        !TODO
-        !work out what the hell that ALLGATHER is doing in comms:initialise_mpi
-        !there are no comments to explain as usual :(
         
     END SUBROUTINE empi_pf_assign_models
     
@@ -182,10 +183,6 @@ CONTAINS
     !-----------------------------------------------------------------------------
 
    SUBROUTINE empi_pf_do_filter()
-        !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        !need to do the random seed thingy... gen_rand:random_seed_mpi(pfid)
-        !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
         IMPLICIT NONE
 
         INTEGER :: iter1, iter2, count, now
